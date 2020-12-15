1b7940b47064f69f0d723a0a0237f9e1320885ff
Jason Bou Kheir <jason.boukheir@pm.me>
2020-12-14 19:30:10 -0800

    feat: :sparkles: Add FunctionPointer compatible Value Delegates
    Unfortunately, these cannot be used in Dotnet.

---
+++ Packages/com.careboo.burst-delegates/CareBoo.Burst.Delegates/SafetyChecks.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Unity.Burst;
+
+namespace CareBoo.Burst.Delegates
+{
+    internal static class SafetyChecks
+    {
+        [BurstDiscard]
+        public static void SupportedInBurstOnly()
+        {
+            throw new NotSupportedException($"Usage of BurstAction and BurstFunc is only supported in Burst-compiled code.");
+        }
+    }
+}
