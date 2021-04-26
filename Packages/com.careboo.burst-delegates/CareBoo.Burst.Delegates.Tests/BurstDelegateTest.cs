6d16ffef727733127a23ae797b142a3b7c8ea079
Jason Bou Kheir <jason.boukheir@pm.me>
2021-04-25 23:04:32 -0700

    feat: :sparkles: Add partial application to Value Delegates
    BREAKING CHANGE: Move compile from Burst delegates to Value delegates

--- Packages/com.careboo.burst-delegates/CareBoo.Burst.Delegates.Tests/BurstDelegateTest.cs
+++ Packages/com.careboo.burst-delegates/CareBoo.Burst.Delegates.Tests/BurstDelegateTest.cs
@@ -1,4 +1,4 @@
-﻿using CareBoo.Burst.Delegates;
+using CareBoo.Burst.Delegates;
 using NUnit.Framework;
 using Unity.Burst;
 using Unity.Collections;
@@ -8,10 +8,10 @@ using Unity.Jobs;
 internal class BurstDelegateTests
 {
     [BurstCompile]
-    public struct CallFuncJob<TImpl> : IJob
-        where TImpl : struct, IFunc<int, int>
+    public struct CallFuncJob<TFunc> : IJob
+        where TFunc : struct, IFunc<int, int>
     {
-        public ValueFunc<int, int>.Impl<TImpl> Func;
+        public ValueFunc<int, int>.Impl<TFunc> Func;
         public NativeArray<int> In;
         public NativeArray<int> Out;
 
@@ -21,29 +21,39 @@ internal class BurstDelegateTests
         }
     }
 
-    [BurstCompile]
-    public static int Add4(int val) => val + 4;
+    public static CallFuncJob<TFunc> NewCallFuncJob<TFunc>(
+        ValueFunc<int, int>.Impl<TFunc> func,
+        NativeArray<int> input,
+        NativeArray<int> output
+    ) where TFunc : struct, IFunc<int, int>
+    {
+        return new CallFuncJob<TFunc> { Func = func, In = input, Out = output };
+    }
 
-    public static readonly BurstFunc<int, int> Add4Func = BurstFunc<int, int>.Compile(Add4);
+    [BurstCompile]
+    public static int Add(int x, int y) => x + y;
+
+    public static readonly ValueFunc<int, int, int>.Impl<BurstFunc<int, int, int>> AddFunc = ValueFunc<int, int, int>.Compile(Add);
+
 
     [Test]
     public void TestCompiledPointerCanInvokeInsideOfBurstedCode()
     {
         using var input = new NativeArray<int>(new int[1], Allocator.Persistent);
         using var output = new NativeArray<int>(new int[1], Allocator.Persistent);
-        var expected = Add4(input[0]);
-        new CallFuncJob<BurstFunc<int, int>> { Func = ValueFunc<int, int>.New(Add4Func), In = input, Out = output }.Run();
+        var expected = Add(4, input[0]);
+        NewCallFuncJob(AddFunc.Apply(4), input, output).Run();
         var actual = output[0];
         Assert.AreEqual(expected, actual);
     }
 
     [Test]
-    public void TestCanInvokeOutsideOfBurstedCode()
+    public void TestCompiledPointerCanInvokeOutsideOfBurstedCode()
     {
         using var input = new NativeArray<int>(new int[1], Allocator.Persistent);
         using var output = new NativeArray<int>(new int[1], Allocator.Persistent);
-        var expected = Add4(input[0]);
-        var actual = Add4Func.Invoke(input[0]);
+        var expected = Add(4, input[0]);
+        var actual = AddFunc.Apply(4).Invoke(input[0]);
         Assert.AreEqual(expected, actual);
     }
 }
